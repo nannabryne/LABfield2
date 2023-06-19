5352b4a74bb42817739901b1c3eaa07271df3afa
nannabryne <nannabryne@outlook.com>
2023-06-19 13:38:11 +0200

    added structure

---
+++ src/boilerplate_examples/README.md
@@ -0,0 +1,9 @@
+>These files contain boilerplate code scripts designed to test different parts of `LATfield2`.
+
+# `field_manipulation.h`
+
+...
+
+# `fft_execution.h`
+
+...
---
+++ src/boilerplate_examples/field_manipulation.hpp
@@ -0,0 +1,18 @@
+/**
+ * @file field_manipulation.hpp
+ * @author Nanna Bryne
+ * 
+ * @brief Boilerplate example: create two fields, initialise them and compute the linear combination
+ * 
+ * @details Uses functions ..... from LATfield2
+*/
+
+#include "LATfield2.hpp"
+using namespace LATfield2;
+
+
+int example(int n, int m){
+
+    parallel.initialise(n, m);
+
+}
