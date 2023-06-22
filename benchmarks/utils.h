3a9c9799fe93f006843a0c62abececa7e6d5e1af
nannabryne <nannabryne@outlook.com>
2023-06-22 11:58:38 +0200

    utils is a bit better

--- benchmarks/utils.h
+++ benchmarks/utils.h
@@ -14,12 +14,22 @@ class Diagnostics{
         string key;     // key descibing unit test
 
         // key properties:
-        int n, m;       // parallel grid size
+        int n, m;           // parallel grid size
+        int num_prcs;       // total number of processes
+        int dim = 3;
+        int num_cells;      // 
+        int num_parts = 0;  // 
+
+        // reference pararameters:
+        int REF_num_prcs;
+        double REF_runtime;
 
         // performance metrics:
+        double runtime;
         double org_runtime;         // 'original' runtime
         double new_runtime;         // current runtime
         double performance_gain;    // gain in performance in % (loss if negative)
+        double efficiency;
 
         // error diagnostics:
         double max_error;   // maximal error (absolute)
@@ -43,12 +53,30 @@ class Diagnostics{
 
         void performance_metrics(double org_time, double new_time);
 
+        void compute_performance_metrics(double comp_duration);
+
+        void provide_reference_parameters(int num_prcs, double comp_duration);
+
+        void provide_computation_parameters(int num_cells_per_dim, int num_parts_per_dim=0, int num_dimensions=3);
+
 };
 
 
 Diagnostics::Diagnostics(string key, int n, int m) : 
-key(key), n(n), m(m){}
+key(key), n(n), m(m){
+    num_prcs = n*m;
+}
 
+void Diagnostics::provide_reference_parameters(int num_prcs, double comp_duration){
+    REF_num_prcs = num_prcs;
+    REF_runtime = comp_duration;
+}
+
+void Diagnostics::provide_computation_parameters(int num_cells_per_dim, int num_parts_per_dim, int num_dimensions){
+    num_cells = num_cells_per_dim;
+    num_parts = num_parts_per_dim;
+    dim = num_dimensions;
+}
 
 
 void Diagnostics::write_epicrisis(){
@@ -61,14 +89,16 @@ void Diagnostics::write_epicrisis(){
                 << endl;
 
     epicrisis << "Key parameters" << endl
-                << "parallel grid: " << "(" << n << ", " << m << ")" << endl
-                << "lattice size: " << "..." << endl
+                << "parallel grid: " << "(" << n << " x " << m << ")" << endl
+                << "lattice size: " << num_cells << "^" << dim << endl
+                << "number of particles: " << num_parts << "^" << dim << endl
                 << endl;
 
     epicrisis << "Performance metrics" << endl
-                << "original runtime: " << org_runtime << " ms" << endl
-                << "latest runtime:   " << new_runtime << " ms" << endl
+                << "original runtime: " << REF_runtime << " ms" << endl
+                << "latest runtime: " << runtime << " ms" << endl
                 << "performance enhancement: " << performance_gain << " %" << endl
+                << "efficiency: " << efficiency << endl
                 << endl;
     
     epicrisis << "Error diagnostics" << endl
@@ -94,13 +124,15 @@ void Diagnostics::print_epicrisis(){ // temporary solution
 
     COUT << "Key parameters" << endl
                 << "parallel grid: " << "(" << n << ", " << m << ")" << endl
-                << "lattice size: " << "..." << endl
+                << "lattice size: " << num_cells << "^" << dim << endl
+                << "number of particles: " << num_parts << "^" << dim << endl
                 << endl;
 
     COUT << "Performance metrics" << endl
-                << "original runtime: " << org_runtime << " ms" << endl
-                << "latest runtime:   " << new_runtime << " ms" << endl
+                << "original runtime: " << REF_runtime << " ms" << endl
+                << "latest runtime: " << runtime << " ms" << endl
                 << "performance enhancement: " << performance_gain << " %" << endl
+                << "efficiency: " << efficiency << endl
                 << endl;
     
     COUT << "Error diagnostics" << endl
@@ -119,13 +151,16 @@ void Diagnostics::error_diagnosis(double max, double min, double avg){
     avg_error = avg;
 }
 
-void Diagnostics::performance_metrics(double org_time, double new_time){
-    org_runtime = org_time;
-    new_runtime = new_time;
-    performance_gain = (org_runtime-new_runtime)/org_runtime*100.0;
-    if(fabs(performance_gain) < 5.){
+
+void Diagnostics::compute_performance_metrics(double comp_duration){
+
+    runtime = comp_duration;
+    performance_gain = (REF_runtime-runtime)/REF_runtime*100.0;
+    if(fabs(performance_gain) < 8.){
         performance_gain = 0.;
     }
+    efficiency = (REF_num_prcs*REF_runtime)/(num_prcs*runtime);
+  
 }
 
 
